477897337aad58f98abf1cd35a77c2dd5d03a00b
amirt <amirtheahmed@gmail.com>
2022-08-21 20:01:50 +0300

    added part 1 courseinfo

---
+++ part1/courseinfo/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+//renders name of course
+const Header = props => {
+    return (
+        <div>
+            <h1>{props.course.name}</h1>
+        </div>
+    );
+};
+
+export default Header;
